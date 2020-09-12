26bf3941cd87bd685badfcc2f836b60278da7e8d
Vignesh2508 <vickky2508@gmail.com>
2020-09-12 10:19:20 +0530

    Update quadtree.py

--- quadtree.py
+++ quadtree.py
@@ -1,5 +1,6 @@
 import random
 import pygame
+import numpy as np
 
 pygame.init()
 
@@ -33,6 +34,7 @@ class Rectangle:
         self.h = h
 
     def contains(self, point):
+        # to check if the point lies inside a rectangle object
         return (point.x >= self.x - self.w) and \
                (point.x <= self.x + self.w) and \
                (point.y <= self.y + self.h) and \
@@ -47,11 +49,13 @@ class QuadTree:
         self.divided = False
 
     def subdivide(self):
+        # to access the objects easily this assignment is made
         x = self.boundary.x
         y = self.boundary.y
         w = self.boundary.w
         h = self.boundary.h
 
+        # Divide the quad into four if the point is in that domain
         ne = Rectangle(x + w / 2, y - h / 2, w / 2, h / 2)
         self.northeast = QuadTree(ne, self.capacity)
         nw = Rectangle(x - w / 2, y - h / 2, w / 2, h / 2)
@@ -66,6 +70,7 @@ class QuadTree:
         if not self.boundary.contains(point):
             return False
 
+        # if the points in a domain is less than the capacity add the point into that domain else subdivide
         if len(self.points) < self.capacity:
             self.points.append(point)
             return True  # append instead of push keyword
@@ -74,7 +79,7 @@ class QuadTree:
                 self.subdivide()
 
             # Need to change this point - need to refactor
-
+            # Made to factor in the Edge cases
             if self.northeast.insert(point):
                 return True
             elif self.northwest.insert(point):
@@ -86,10 +91,11 @@ class QuadTree:
 
     def show(self):
 
-        pygame.draw.rect(win, white, (self.boundary.x - self.boundary.w, self.boundary.y - self.boundary.h,
-                                      self.boundary.w * 2, self.boundary.h * 2), 1)
+        pygame.draw.rect(win, red, (self.boundary.x - self.boundary.w, self.boundary.y - self.boundary.h,
+                                    self.boundary.w * 2, self.boundary.h * 2), 1)
 
         if self.divided:
+            # Recursive call to display the rectangles
             self.northeast.show()
             self.northwest.show()
             self.southeast.show()
@@ -103,13 +109,22 @@ class QuadTree:
 
 def main():
     boundary = Rectangle(400, 400, 400, 400)
-    qtree = QuadTree(boundary, 2)
+    qtree = QuadTree(boundary, 1)
+    # filename = 'airfoil_2.txt'
+    # indata = np.loadtxt(filename, usecols=(0, 1))  # make sure the rest is ignored
     i = 0
-    while i < 500:
+    while i < 800:
         p = Point(random.randint(1, 799), random.randint(1, 799))
+        # p = Point(int(indata[i][0])*2 - 200, int(indata[i][1])*2-100)
         qtree.insert(p)
         i = i + 1
     qtree.show()
+    while i < 1500:
+        # To stop pygame from closing the window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                i = 2000
+        pygame.display.update()
 
 
 main()
